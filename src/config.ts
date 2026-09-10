377ac94525fad7f12fe86a86f1ed6f747304371b
RealAbdurRehman <claudethebitplayer@gmail.com>
2026-09-10 17:40:31 +0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,9 +11,10 @@ const config = {
   collisionDamping: 1,
   particleSize: 0.5,
   cameraDistance: 50,
-  boundsSize: 30,
+  boundsSize: 50,
   numParticles: 1,
   particleSpacing: 0,
+  smoothingRadius: 1,
 };
 
 const simulationFolder = gui.addFolder("Simulation");
@@ -21,11 +22,11 @@ simulationFolder.add(config, "paused");
 simulationFolder.add(config, "gravity", -100, 100);
 simulationFolder.add(config, "collisionDamping", 0, 1);
 simulationFolder
-  .add(config, "boundsSize", 10, 80)
+  .add(config, "boundsSize", 30, 150)
   .onChange((size: number) => setBoundsSize(size));
 
 const particleFolder = gui.addFolder("Particles");
-particleFolder.add(config, "numParticles", 1, 100, 1).onChange(() => start());
+particleFolder.add(config, "numParticles", 1, 500, 1).onChange(() => start());
 particleFolder
   .add(config, "particleSpacing", 0, 4)
   .onChange(() => setParticleGridPosition());
@@ -33,10 +34,11 @@ particleFolder.add(config, "particleSize", 0.1, 10).onChange((size: number) => {
   setParticleSize(size);
   setParticleGridPosition();
 });
+particleFolder.add(config, "smoothingRadius", 1, 5);
 
 const cameraFolder = gui.addFolder("Camera");
 cameraFolder
-  .add(config, "cameraDistance", 10, 100)
+  .add(config, "cameraDistance", 10, 200)
   .onChange((distance: number) => setCameraDistance(distance));
 
 export { config };
